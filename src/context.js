9557ac9482829fcbc3ef3618d63db0b4d48ca990
ashmik <ashmik.harinkhede@gmail.com>
2021-11-10 17:30:38 +0530

    Next and Prev pages

--- src/context.js
+++ src/context.js
@@ -44,14 +44,20 @@ const AppProvider = ({ children }) => {
 
   const handleSearch = (query) => { 
     dispatch({type:HANDLE_SEARCH, payload:query})
+  }  
+
+  const handlePage = (value) => {   
+    // console.log(value)
+    // dispatch 
+    dispatch({type:HANDLE_PAGE, payload:value})
   }
 
   useEffect(() => {
     fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`);
-  }, [state.query]);
+  }, [state.query, state.page]);
 
   return (
-    <AppContext.Provider value={{ ...state, removeStory, handleSearch }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ ...state, removeStory, handleSearch, handlePage }}>{children}</AppContext.Provider>
   );
 };
 // make sure use
